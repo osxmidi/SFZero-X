047e42bec14dd51eb3dcb6b43e1dd8a083367e9f
osxmidi <chopinbig@tutanota.com>
2020-05-01 15:11:51 +1000

    Update JucePluginDefines.h

--- plugin/JuceLibraryCode/JucePluginDefines.h
+++ plugin/JuceLibraryCode/JucePluginDefines.h
@@ -164,5 +164,5 @@
  #define JucePlugin_MaxNumOutputChannels   2
 #endif
 #ifndef  JucePlugin_PreferredChannelConfigurations
- #define JucePlugin_PreferredChannelConfigurations  {1, 1}, {2, 2}
+ #define JucePlugin_PreferredChannelConfigurations  {0, 1}, {0, 2}
 #endif
